cb467ff9f36066aa05a25084d28d5b4dda766d87
Aroldas <90574359+unfol@users.noreply.github.com>
2024-05-21 21:07:27 +0300

    Background color (gradient), leasing edit, validation.

--- app/Http/Controllers/LeasingController.php
+++ app/Http/Controllers/LeasingController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LeasingController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['only' => ['destroy', 'edit', 'create', 'store', 'update']]);
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -56,21 +61,43 @@ class LeasingController extends Controller
     {
         $leasing = Leasing::first();
         $request->validate([
-            'min_amount' => 'required|numeric',
-            'max_amount' => 'required|numeric',
-            'min_down_payment' => 'required|numeric',
-            'max_down_payment' => 'required|numeric',
-            'min_time_period' => 'required|numeric',
-            'max_time_period' => 'required|numeric',
-            'min_interest_rate' => 'required|numeric',
-            'max_interest_rate' => 'required|numeric',
+            'min_amount' => 'required|numeric|max:'.$request->max_amount,
+            'max_amount' => 'required|numeric|min:'.$request->min_amount,
+            'min_down_payment' => 'required|numeric|max:'.$request->max_down_payment,
+            'max_down_payment' => 'required|numeric|min:'.$request->min_down_payment,
+            'min_time_period' => 'required|numeric|max:'.$request->max_time_period,
+            'max_time_period' => 'required|numeric|min:'.$request->min_time_period,
+            'min_interest_rate' => 'required|numeric|max:'.$request->max_interest_rate,
+            'max_interest_rate' => 'required|numeric|min:'.$request->min_interest_rate,
             'administration_fee' => 'required|numeric',
+        ],
+        [
+            'min_amount.max' => 'The minimum amount must be less than the maximum amount.',
+            'max_amount.min' => 'The maximum amount must be greater than the minimum amount.',
+            'min_down_payment.max' => 'The minimum down payment must be less than the maximum down payment.',
+            'max_down_payment.min' => 'The maximum down payment must be greater than the minimum down payment.',
+            'min_time_period.max' => 'The minimum time period must be less than the maximum time period.',
+            'max_time_period.min' => 'The maximum time period must be greater than the minimum time period.',
+            'min_interest_rate.max' => 'The minimum interest rate must be less than the maximum interest rate.',
+            'max_interest_rate.min' => 'The maximum interest rate must be greater than the minimum interest rate.',
         ]);
         $input = $request->all();
         $input['min_down_payment'] = $input['min_down_payment'] / 100;
         $input['max_down_payment'] = $input['max_down_payment'] / 100;
         $input['min_interest_rate'] = $input['min_interest_rate'] / 100;
         $input['max_interest_rate'] = $input['max_interest_rate'] / 100;
+
+        if ($leasing->min_amount == $input['min_amount'] &&
+            $leasing->max_amount == $input['max_amount'] &&
+            $leasing->min_down_payment == $input['min_down_payment'] &&
+            $leasing->max_down_payment == $input['max_down_payment'] &&
+            $leasing->min_time_period == $input['min_time_period'] &&
+            $leasing->max_time_period == $input['max_time_period'] &&
+            $leasing->min_interest_rate == $input['min_interest_rate'] &&
+            $leasing->max_interest_rate == $input['max_interest_rate'] &&
+            $leasing->administration_fee == $input['administration_fee']) {
+            return redirect()->route('leasing.edit');
+        }
 
         $leasing->update($input);
         $request->session()->flash('success', 'Update successful!');
