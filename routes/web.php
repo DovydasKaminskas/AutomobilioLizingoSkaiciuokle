3eaf31b86506cbfb38f85b6ac966f16d3146b798
Dovydas <dovydas.kaminskas@ku.lt>
2024-05-22 19:24:10 +0300

    leasing/update route

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/leasing', [LeasingController::class, 'index'])->name('leasing.index');
 Route::get('/leasing/edit', [LeasingController::class, 'edit'])->name('leasing.edit');
 Route::put('/leasing/update', [LeasingController::class, 'update'])->name('leasing.update');
+
+Route::get('/leasing/update', function () {
+    abort(404);
+});
